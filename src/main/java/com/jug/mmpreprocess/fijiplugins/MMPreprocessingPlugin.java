eaf7ddc97be04cbed045dbbc16f5e3487de337f1
haesleinhuepf <rhaase@scicomp-mac-2-tb.mpi-cbg.de>
2016-10-21 15:14:46 +0200

    removed dead code, added comments

--- src/main/java/com/jug/mmpreprocess/fijiplugins/MMPreprocessingPlugin.java
+++ src/main/java/com/jug/mmpreprocess/fijiplugins/MMPreprocessingPlugin.java
@@ -18,6 +18,8 @@ public class MMPreprocessingPlugin implements PlugIn {
         String currentDir = Prefs.getDefaultDirectory();
 
 
+        // -----------------------------------------
+        // plugin configuration
         GenericDialogPlus gd = new GenericDialogPlus("MMPreprocessing Configuration");
         gd.addDirectoryField("Input_folder", currentDir);
         gd.addDirectoryField("Output_folder", currentDir);
@@ -58,28 +60,15 @@ public class MMPreprocessingPlugin implements PlugIn {
                 "" + (timePointStartIndex + numberOfTimePoints - 1)
         };
 
+        // -----------------------------------------
+        // for tracing
         for (String param : args) {
             IJ.log("mmp params " + param);
         }
 
-
-                /*new String[13];
-        args[0] = "mmpreprocess";
-        args[1] = "-i";
-        args[2] = "/Users/rhaase/Projects/Florian_Jug_Myers_MoMA_Deployment/data/registered2";
-        args[3] = "-o";
-        args[4] = "/Users/rhaase/Projects/Florian_Jug_Myers_MoMA_Deployment/data/splitted";
-        args[5] = "-c";
-        args[6] = "2";
-        args[7] = "-cmin";
-        args[8] = "1";
-        args[9] = "-tmin";
-        args[10] = "1";
-        args[11] = "-tmax";
-        args[12] = "40";
-        */
+        // -----------------------------------------
+        // Actually run MMPreprocess
         MMPreprocess.running_as_Fiji_plugin = true;
-
         MMPreprocess.main(args);
     }
 
